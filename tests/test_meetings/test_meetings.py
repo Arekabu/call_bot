0dd4551f032e90f0be9e48e7fca33db0aa9d320b
Якомазов Алексей <a.yakomazov@ylab.team>
2025-12-19 12:50:47 +0000

    [feature] Добавлен тест обработки пустого url [QNUL-146] [prod]

--- tests/test_meetings/test_meetings.py
+++ tests/test_meetings/test_meetings.py
@@ -12,7 +12,12 @@ async def test_get_meetings_success(meetings_api, mock_callback, meetings_servic
     mock_callback.message.answer.assert_called_once()
     text = mock_callback.message.answer.call_args[0][0]
 
-    assert "Test Meeting" in text
+    assert "Test Meeting 01" in text
     assert "10:00" in text
     assert "11:00" in text
     assert "https://test.meet/123" in text
+
+    assert "Test Meeting 02" in text
+    assert "12:00" in text
+    assert "13:00" in text
+    assert "Ссылка не предоставлена." in text
